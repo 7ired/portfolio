f07135c844b1292ff96d62cf19948cf891460642
Eryk Witkowski <erykwitkowski2@gmail.com>
2023-10-14 20:33:47 +0200

    Restructured everything with blueprints

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,14 +2,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from blog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '0dfbfacdd519616551a22afc7be0ca91'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from blog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from blog.users.routes import users
+    from blog.posts.routes import posts
+    from blog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
--- blog/users/forms.py
+++ blog/users/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from blog.models import User
 from flask_login import current_user
+from blog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -54,8 +54,19 @@ class UpdateAccountForm(FlaskForm):
             if email:
                 raise ValidationError('User with this email is already registered. Forgot password?')
             
-class PostForm(FlaskForm):
-    title = StringField('Title',
-                        validators=[DataRequired(), Length(max=100)])
-    content = TextAreaField('Content', validators=[DataRequired(), Length(max=2000)])
-    submit = SubmitField('Post')
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
