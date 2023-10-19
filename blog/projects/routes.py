5654b8d1b2911de8b5656c713d6a04e639c08a7a
Eryk Witkowski <erykwitkowski2@gmail.com>
2023-10-19 21:18:49 +0200

    finished projects page

--- blog/projects/routes.py
+++ blog/projects/routes.py
@@ -10,53 +10,66 @@ projects_data = [
 		'title': 'Monte Carlo Stock Simulation',
 		'description': 'Stock prices follow a random walk, which makes them unpredictable; we can only apply different strategies and try to forecast the returns. However, there is a simple tool to simulate how a portfolio could behave throughout a certain timeframe. Monte Carlo Simulation of the S&P 500 index as well as a calculation of Value at Risk and Conditional Value at Risk statistics.',
 		'image': 'montecarlo.jpeg',
-		'id': 'montecarlo'
+		'id': 'montecarlo',
+		'tags': 'Finance, Maths, Statistics, Data Visualization'
 	},
 	{
 		'title': 'Polish Electricity Market Statistical Analysis',
 		'description':
-		"""
-The analysis was based on average daily market electricity prices. Prior to June 2021, the market was stable and prices were seasonal, until the war in the east made electricity prices variable and noisy. In the process, I tried forecasting the prices with different approaches like Simple Exponential Smoothing, AR, ARMA models, and a hybrid gradient-boosting regression model. I've also conducted a seasonal Fourier analysis. Raw data was taken from PSE S.A. and prepared by myself.
-""",
+		"""The analysis was based on average daily market electricity prices. Prior to June 2021, the market was stable and prices were seasonal, until the war in the east made electricity prices variable and noisy. In the process, I tried forecasting the prices with different approaches like Simple Exponential Smoothing, AR, ARMA models, and a hybrid gradient-boosting regression model. I've also conducted a seasonal Fourier analysis. Raw data was taken from PSE S.A. and prepared by myself.""",
 		'image': 'electricity.jpg',
-		'id': 'electricity'
+		'id': 'electricity',
+		'tags': 'Time Series Forecasting, Data Manipulation, Data Gathering, Exploratory Data Analysis, Feature Engineering, Statistics, Machine Learning, XGBoost'
 	},
 	{
 		'title': 'Markov Chains Stock Movements Prediction',
 		'description': 'The Markov Chains Stock Movements Prediction project is designed to forecast future stock price movements using the principles of Markov Chains. Leveraging historical stock price data, this project employs a probabilistic model to make predictions about the direction of stock movements over a defined period.',
 		'image': 'chains.png',
-		'id': 'chains'
+		'id': 'chains',
+		'tags': 'Finance, Maths, Probabilistics, Statistics, Data Manipulation'
 	},
 	{
 		'title': 'LSTM Neural Network Stock Prediction with TensorFlow',
 		'description': 'The LSTM Neural Network Stock Prediction project utilizes TensorFlow, a powerful machine learning library, to develop a predictive model for forecasting stock prices. Specifically, it employs Long Short-Term Memory (LSTM) networks, a type of recurrent neural network (RNN), known for their ability to capture sequential patterns in time series data. Project is based on S&P500 index.',
 		'image': 'rnn.png',
-		'id': 'rnn'
+		'id': 'rnn',
+		'tags': 'Finance, Maths, Data Manipulation, Data Gathering, Exploratory Data Analysis, Feature Engineering, Data Visualiation, Deep Learning, TensorFlow'
 	},
 	{
 		'title': 'Twitch.tv Data Streaming and Database Integration Project',
 		'description': 'The Twitch.tv Data Streaming and Database Integration project is a comprehensive endeavor focused on gathering real-time data from the popular streaming platform, Twitch.tv. Leveraging APIs provided by Twitch, this project aims to collect, process, and store a wide array of data related to streamers, channels, viewership, and engagement metrics.',
 		'image': 'ttv.png',
-		'id': 'ttv'
+		'id': 'ttv',
+		'tags': 'API, Data Gathering, Data Manipulation, Databases, json'
 	},
 	{
 		'title': 'Data Science London Meetup - Sklearn Fun',
 		'description': 'Data Science London is hosting a meetup on Scikit-learn.  This competition is a practice ground for trying, sharing, and creating examples of sklearns classification abilities (if this turns in to something useful, we can follow it up with regression, or more complex classification problems).',
 		'image': 'sklearn.jpg',
-		'id': 'sklearn'
+		'id': 'sklearn',
+		'tags': 'Sklearn, Data Manipulation, Machine Learning, Feature Selection, Cross Validation'
 	},
 	{
 		'title': 'Berlin Airbnb Price Predictions',
 		'description': 'The Berlin Airbnb Price Prediction estimates the nightly rental prices of listings on the Airbnb platform in Berlin, Germany. This project aims to offer hosts and travelers valuable insights into the expected costs of accommodations in various neighborhoods and property types across Berlin. This is my first machine learning project ever done.',
 		'image': 'berlin.jpg',
-		'id': 'berlin'
+		'id': 'berlin',
+		'tags': 'Data Manipulation, Data Visualization, Exploratory Data Analysis, Feature Engineering, Deep Learning, Neural Networks'
 	},
 	{
 		'title': 'LeMans 24h Race Winners',
 		'description': 'Web Scraping, Data Cleaning and Feature Engineering raw data about the LeMans 24h Race Winners. Aim of this task was to get a clean dataset to run visualizations.',
 		'image': 'lemans.jpg',
-		'id': 'lemans'
+		'id': 'lemans',
+		'tags': 'Data Gathering, Data Manipulation, BeautifulSoup'
 	},
+	{
+		'title': 'Spaceship Titanic',
+		'description': 'Predict which passengers are transported to an alternate dimension. Kaggle competition.',
+		'image': 'spaceship.jpg',
+		'id': 'spaceship',
+		'tags': 'Data Manipulation, Data Visualization, Exploratory Data Analysis, Logistic Regression'
+	}
 
 	# Add more projects as needed
 ]
