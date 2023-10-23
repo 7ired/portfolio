4a4afc88dd42ea2685dbb34dcaa51d5948bfff45
Eryk Witkowski <erykwitkowski2@gmail.com>
2023-10-23 20:25:12 +0200

    db transfer

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") 
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
