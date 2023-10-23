d802eb756512679f006e7634b6361677e291b1bb
Eryk Witkowski <erykwitkowski2@gmail.com>
2023-10-23 21:41:43 +0200

    trying to fix pictures

--- blog/users/routes.py
+++ blog/users/routes.py
@@ -7,22 +7,6 @@ from blog.users.utils import save_picture
 
 users = Blueprint('users', __name__)
 
-@users.route("/register", methods=['GET', 'POST'])
-def register():
-	if current_user.is_authenticated:
-		return redirect(url_for('main.home'))
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user = User(username=form.username.data,
-              		email=form.email.data,
-              		password=hashed_password)
-		db.session.add(user)
-		db.session.commit()
-		flash(f'Your account has been created. You can now log in!', 'success')
-		return redirect(url_for('users.login'))
-	return render_template('register.html', title='Register', form=form)
-
 @users.route("/login", methods=['POST', 'GET'])
 def login():
 	if current_user.is_authenticated:
