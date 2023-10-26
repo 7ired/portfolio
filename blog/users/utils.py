5e9ec1d5e01bcd750f0ca4333466d4190dbdf733
Eryk Witkowski <erykwitkowski2@gmail.com>
2023-10-26 20:45:50 +0200

    fixing the picture

--- blog/users/utils.py
+++ blog/users/utils.py
@@ -5,11 +5,11 @@ from flask import url_for, current_app
 from flask_login import current_user
 from flask_mail import Message
 from blog import mail
+from werkzeug.utils import secure_filename
 
 def save_picture(form_picture):
-	random_hex = secrets.token_hex(8)
 	_, f_ext = os.path.splitext(form_picture.filename)
-	picture_fn = random_hex + f_ext
+	picture_fn = secure_filename(form_picture.filename)  # Use secure_filename to ensure safe filenames
 	picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
 	form_picture.save(picture_path)
 	
